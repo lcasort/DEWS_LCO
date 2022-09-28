b709ea503090c1b98a8b4fc78782682059333770
lcasort <laucasort.notes@gmail.com>
2022-09-28 19:02:23 +0200

    main.php: formulario que muestra datos en la misma página.

---
+++ pruebasPHP/formulario/main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Form</title>
+    </head>
+    <body>
+    <?php 
+        if(empty($_POST) || empty($_POST["name"])) { ?>
+            <form method="post">
+            <label for="name">Nombre alumno:</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="modulo">Módulo que cursa:</label>
+            <select name="modulo" id="modulo">
+                <option value="Desarrollo Web en Entorno Servidor" selected>Desarrollo Web en Entorno Servidor</option>
+                <option value="Desarrollo Web en Entorno Cliente">Desarrollo Web en Entorno Cliente</option>
+            </select><br>
+            <input type="submit" value="Enviar" />
+            </form>
+        <?php 
+        } else {
+            echo "<p><pre>";
+            print_r($_POST);
+            echo "</pre></p>";
+        }
+        ?>
+    </body>
+</html>
--- pruebasPHP/formulario/procesa.php
+++ pruebasPHP/formulario/procesa.php
@@ -2,6 +2,8 @@
 <html>
     <body>
     Alumno: <?php echo $_POST["name"]; ?><br>
-    Módulo: <?php echo $_POST["modulo"] ?>
+    Módulo: <?php echo $_POST["modulo"]; ?>
+    <p><pre><?php print_r($_POST); ?></pre></p>
+    <p><pre><?php print_r($_GET); ?></pre></p>
     </body>
 </html>
