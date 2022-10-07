8a6196d7b7800f86741090209cc408273e28c2a0
lcasort <laucasort.notes@gmail.com>
2022-10-07 20:17:43 +0200

    Enseñas posibles números casi finalizado. Falta sacar las opciones por pantalla.

--- Castillo_Ortiz_Laura_DWES_Tarea01/Ejercicio_2/actions.php
+++ Castillo_Ortiz_Laura_DWES_Tarea01/Ejercicio_2/actions.php
@@ -103,3 +103,125 @@ function delete($label, $sudoku, $row, $column) {
 
     echo $result;
 }
+
+function showOptions($label, $sudoku, $row, $column) {
+    $numeros = array(1,2,3,4,5,6,7,8,9);
+
+    foreach($numeros as $n) {
+        $columna = selectColumn($sudoku, $column);
+        $square = selectSquare($sudoku, $row, $column);
+        $linea = comprobarLinea($n, $sudoku[$row-1]);
+        $columna = comprobarColumna($n, $columna);
+        $cuadrado = comprobarCuadrado($n, $square);
+
+    }
+
+    $result = '<div class="bloque">';
+    $result .= '<h2>' . $label . '</h2>';
+    $result .= '<table>';
+    for($i=0; $i<count($sudoku); $i++) {
+        $result .= '<tr>';
+        
+        for($j=0; $j< sizeof($sudoku[$i]); $j++) {
+            if($sudoku[$i][$j]===0) {
+                $result .= '<th style="color: blue;">.</td>';
+            } else {
+                $result .= '<th style="color: red;">' . $sudoku[$i][$j] . '</td>';
+            }
+        }
+        
+        $result .= '</tr>';
+    }
+    
+    $result .= '</table>';
+    $result .= '</div>';
+    $result .= '</div>';
+    $result .= '<div class="form">';
+    $result .= '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
+    $result .= '<div class="inputs">';
+    $result .= '<input type="hidden" name="dificultad" value="' . $label . '" />';
+    $result .= '<input type="hidden" name="sudoku" value="' . base64_encode(serialize($sudoku)) . '" />';
+
+    echo $result;
+}
+
+function comprobarLinea($n, $row) {
+    $res = true;
+    for($i=0; $i<=sizeof($row)-1; $i++) {
+        if($n == $row[$i]) {
+            $res = false;
+            break;
+        }
+    }
+
+    return $res;
+}
+
+function selectColumn($sudoku, $column) {
+    $res = array();
+    for($i=0; $i<=sizeof($sudoku[0])-1; $i++) {
+        array_push($res, $sudoku[$i][$column]);
+    }
+
+    return $res;
+}
+
+function comprobarColumna($n, $columna) {
+    $res = true;
+    for($i=0; $i<=sizeof($columna)-1; $i++) {
+        if($n == $columna[$i]) {
+            $res = false;
+            break;
+        }
+    }
+
+    return $res;
+}
+
+function selectSquare($sudoku, $row, $column) {
+    $minRow = 0;
+    $maxRow = 0;
+    $minColumn = 0;
+    $maxColumn = 0;
+    if($row>=0 && $row<=2) {
+        $minRow = 0;
+        $maxRow = 2;
+    } elseif($row>=3 && $row<=5) {
+        $minRow = 3;
+        $maxRow = 5;
+    } else {
+        $minRow = 6;
+        $maxRow = 8;
+    }
+    if($column>=0 && $column<=2) {
+        $minColumn = 0;
+        $maxColumn = 2;
+    } elseif($column>=3 && $column<=5) {
+        $minColumn = 3;
+        $maxColumn = 5;
+    } else {
+        $minColumn = 6;
+        $maxColumn = 8;
+    }
+
+    $res = array();
+    for($i=$minRow; $i<=$maxRow; $i++) {
+        for($j=$minColumn; $j<=$maxColumn; $j++) {
+            array_push($res, $sudoku[$i][$j]);
+        }
+    }
+
+    return $res;
+}
+
+function comprobarCuadrado($n, $square) {
+    $res = true;
+    for($i=0; $i<sizeof($square)-1; $i++) {
+        if($n == $square[$i]) {
+            $res = false;
+            break;
+        }
+    }
+
+    return $res;
+}
