87f7b4f34a2321bb8d7dc2ff32af54e347716a55
lcasort <laucasort.notes@gmail.com>
2022-10-18 17:20:09 +0200

    Main HTML done.

--- ejerciciosTema2/cambiarNombreDep.php
+++ ejerciciosTema2/cambiarNombreDep.php
@@ -6,38 +6,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACME</title>
     <style>
-        table, tr {
-            border:1px solid black;
-            border-collapse: collapse;
+        .mainContainer {
+            margin: 0 auto;
+        }
+
+        .regContainer {
+            border-right: solid 1px black;
+            display: inline-block;
+        }
+
+        .addContainer {
+            margin: 20px 0;
+        }
+        
+        .updContainer {
+            display: inline-block;
         }
     </style>
 </head>
 <body>
-    <h1>DEPARTAMENTOS</h1>
+    <div class="mainContainer">
+        <h1>DEPARTAMENTOS</h1>
+    </div>
 
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">        
-        <?php
-            @ $conexion = new mysqli('localhost', 'root', '', 'employees');
+    <div class="form">
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
-            $error = $conexion->connect_errno;
-            
-            if ($error == null) {
-            
-                $resultado = $conexion->query('SELECT * FROM departments');
-                $departamentos = $resultado->fetch_array();
+            <div class="addContainer">
+                <input type="submit" value="+" name="add" />
+                <input type="text" id="newDptName" placeholder="Nuevo departamento" />
+            </div>
 
-                while ($departamentos != null) {
-                    if(isset($_POST['submit'])) {
-                        $resultado = $conexion->query('UPDATE departments SET dept_name = ' . $_POST[$departamentos['dept_no']] . 'WHERE ' . $departamentos['dept_name'] . '==' .$_POST[$departamentos['dept_no']]);
-                    }
-                    echo '<p>';
-                    echo '<input type="text" name="' . $departamentos['dept_no'] . '" id="nameDpt" value="' . $departamentos['dept_name'] . '" />';
-                    echo '</p>';
+            <?php
+                @ $conexion = new mysqli('localhost', 'root', '', 'employees');
+
+                $error = $conexion->connect_errno;
+                
+                if ($error == null) {
+                
+                    $resultado = $conexion->query('SELECT * FROM departments');
                     $departamentos = $resultado->fetch_array();
-                }            
-            }
-        ?>
-        <input type="submit" value="Enviar" name="submit" />
-    </form>
+
+                    echo '<div class="regContainer">';
+                    while ($departamentos != null) {
+                        echo '<input type="submit" id="delete" value="X" name="delete_'
+                        . $departamentos['dept_no'] . ' />';
+                        echo '<input type="text" name="' . $departamentos['dept_no']
+                        . '" id="nameDpt" value="' . $departamentos['dept_name'] . '" />';
+                        echo '<br>';
+                        $departamentos = $resultado->fetch_array();
+                    }
+                    echo '</div>';
+                }
+            ?>
+
+            <div class="updContainer">
+                <input type="submit" value="Refrerscar" name="update" />
+            </div>
+
+        </form>
+    </div>
+
 </body>
 </html>
