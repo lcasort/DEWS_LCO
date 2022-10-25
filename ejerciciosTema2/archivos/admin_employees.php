2bf280b6a729dbddd8ce54189124436d9287372b
lcasort <laucasort.notes@gmail.com>
2022-10-25 18:15:12 +0200

    Exercise 1.

---
+++ ejerciciosTema2/archivos/admin_employees.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin DB</title>
+    <style>
+        .msg {
+            color: red;
+            font-size: small;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+    // Abrimos la conexión con la base de datos
+    $conexion = new mysqli('localhost', 'root', '', 'employees');
+
+    $msg = '';
+
+    $error = $conexion->connect_errno;
+    $error_message = "";
+    if ($error != 0) {
+        ?>
+            <p>Error de conexión a la base de datos. Texto del error: <?php echo $conexion->connect_error; ?></p>;
+        <?php 
+            exit();
+        } else {
+            if (isset($_POST)&&!empty($_POST)) {
+                if (isset($_POST['reboot'])) {
+                    /*
+                    sudo chgrp -R www-data /var/www/html
+                    sudo gpasswd -a username www-data
+                    sudo chmod -R 777 /var/www/html
+
+
+                    sudo chown -R www-data:${USER} html
+                    */
+                    exec('sudo chown -R www-data:${USER} /opt/lampp/bin/mysql && cd /home/laucasort/Downloads/test_db-master && sudo /opt/lampp/bin/mysql < employees.sq');
+                    // $sql = readfile('/home/laucasort/Downloads/test_db-master/employees.sql');
+                    // mysqli_multi_query($conexion,$sql);
+                    // /home/laucasort/Downloads/test_db-master
+                    // sudo /opt/lampp/bin/mysql < employees.sql
+                    $msg = 'Reboot completed.';
+                }
+            }
+        ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <input type="submit" name="reboot" value="Reboot">
+    </form>
+    <span class="msg"><?php echo $msg; ?></span>
+    <?php 
+        $conexion->close();
+    }
+    ?>
+</body>
+</html>
