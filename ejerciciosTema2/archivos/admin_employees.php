bc3be4491532375079d1400cc4ec0579f979f40e
lcasort <laucasort.notes@gmail.com>
2022-10-26 18:41:19 +0200

    Reboot employees database.

--- ejerciciosTema2/archivos/admin_employees.php
+++ ejerciciosTema2/archivos/admin_employees.php
@@ -33,30 +33,7 @@
         } else {
             if (isset($_POST)&&!empty($_POST)) {
                 if (isset($_POST['reboot'])) {
-                    /*
-                    sudo chgrp -R www-data /var/www/html
-                    sudo gpasswd -a username www-data
-                    sudo chmod -R 777 /var/www/html
-
-
-                    sudo chown -R www-data:${USER} html
-                    */
-                    // echo exec('whoami');
-                    // echo exec('chgrp -R www-data /opt/lampp/bin/mysql');
-                    // echo exec('ls -laF /opt/lampp/bin/mysql');
-                    // $command = '/opt/lampp/bin/mysql';
-                    // echo exec('/opt/lampp/bin/mysql -u root -p employees < employees.sql', $out);
-                    // var_dump($out);
-                    $dir = fopen('/home/laucasort/Downloads', 'rx');
-                    exec("ls /home/laucasort", $out);
-                    var_dump($out);
-                    echo shell_exec("ls ~/Downloads");
-                    var_dump($outp);
-                    // echo exec('cd /home/laucasort/Downloads/test_db-master && sudo /opt/lampp/bin/mysql -u root -p < employees.sql');
-                    // $sql = readfile('/home/laucasort/Downloads/test_db-master/employees.sql');
-                    // mysqli_multi_query($conexion,$sql);
-                    // /home/laucasort/Downloads/test_db-master
-                    // sudo /opt/lampp/bin/mysql < employees.sql
+                    exec('cd ./test_db-master && /opt/lampp/bin/mysql -u root < employees.sql');
                     $msg = 'Reboot completed.';
                 }
             }
