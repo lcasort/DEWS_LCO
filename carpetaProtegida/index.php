39d8d25273384aee58ef61ab5554cbbdb6ebe611
lcasort <laucasort.notes@gmail.com>
2022-11-02 20:01:29 +0100

    Restringir acceso a carpeta.

--- carpetaProtegida/index.php
+++ carpetaProtegida/index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola mundo!</h1>
-</body>
+<html>
+    <head>
+        <title>Hola mundo</title>
+    </head>
+
+    <body>
+        <!--
+            Para Ubuntu:
+                OP. 1:
+                    sudo /opt/lampp/bin/htpasswd -c /opt/lampp/htdocs/DEWS_LCO/carpetaProtegida/users dwes
+                OP. 2:
+                    cd /opt/lampp/htdocs/DEWS_LCO/carpetaProtegida
+                    sudo /opt/lampp/bin/htpasswd -c users dwes
+
+            Para Windows:
+                cd C:\xampp\htdocs\DEWS_LCO\carpetaProtegida
+                C:\xampp\mysql\bin\.\htpasswd -c users dwes
+
+            PD: La opción -c solo se utiliza la primera vez que vamos a meter a un usuario.
+        -->
+        <h1>Hola mundo!</h1>
+    </body>
 </html>
