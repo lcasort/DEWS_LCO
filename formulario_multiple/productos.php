6fcd4122fb60153ba9224b957e5c1ca6d51e7376
lcasort <laucasort.notes@gmail.com>
2022-11-24 18:18:15 +0100

    Paginación.

--- formulario_multiple/productos.php
+++ formulario_multiple/productos.php
@@ -11,6 +11,8 @@
         unset($_SESSION['total']);
     }
 
+    $htmlText = '';
+
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -18,7 +20,7 @@
     $conexion = new mysqli('localhost', 'root', '', 'ejtienda');
 
     $error = $conexion->connect_errno;
-    $error_message = ""; 
+    $error_message = "";
 
     if ($error != 0) {
 
@@ -27,26 +29,48 @@
 
     } else {
 
-        if(isset($_POST['cart']) && !empty($_POST['cart'])) {
-
-            $values = '(\'' . implode("','", array_keys($_POST['cart'])) . '\')';
-            $res = $conexion -> query("SELECT cod FROM producto WHERE cod IN $values");
-
-            while($cod = $res -> fetch_array()) {
-                if(!isset($_SESSION['cart'])) {
-                    $_SESSION['cart'] = array();
-                    array_push($_SESSION['cart'], $cod['cod']);
-                } else {
-                    if (!in_array($cod['cod'], $_SESSION['cart'])) {
-                        array_push($_SESSION['cart'], $cod['cod']);
-                    }
-                }
-            }
-                
-            header('Location: ./cesta.php');
-            exit();
-
+        // Obtenemos el número de la página en la que estamos.
+        if (isset($_GET['page_no']) && !empty($_GET['page_no'])) {
+            $page_no = $_GET['page_no'];
+            $_SESSION['page'] = $_GET['page_no'];
         } else {
+            $page_no = 1;
+            $_SESSION['page'] = $page_no;
+        }
+
+        // Guardamos el número de productos que queremos enseñar.
+        $total_records_per_page = 3;
+
+        // Calculamos el offset
+        $offset = ($page_no-1) * $total_records_per_page;
+
+        // Calculámos los números de las páginas anterior y posterior.
+        $previous_page = $page_no - 1;
+        $next_page = $page_no + 1;
+        $adjacents = "2";
+
+        // Obtenemos el total de entradas en la tabla.
+        $result_count = $conexion -> query("SELECT COUNT(*) AS total_records FROM producto");
+        $total_records = $result_count -> fetch_array();
+        $total_records = $total_records['total_records'];
+        // Calculamos el número total de páginas que vamos a tner.
+        $total_no_of_pages = ceil($total_records / $total_records_per_page);
+        // Número total de páginas menos 1.
+        $second_last = $total_no_of_pages - 1;
+
+        // Seleccionamos las entradas que queremos enseñar y guardamos el html en
+        // una variable.
+        $result = $conexion -> query("SELECT * FROM producto LIMIT $offset, $total_records_per_page");
+        while($row = $result -> fetch_array()){
+            $htmlText .= '<tr>';
+            $htmlText .= '<td>'.$row['nombre_corto'].'</td>';
+            $htmlText .= '<td>'.$row['descripcion'].'</td>';
+            $htmlText .= '<td>'.$row['PVP'].'€</td>';
+            $htmlText .= '</tr>';
+                }
+
+        $conexion -> close();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -64,48 +88,39 @@
         <a href="./logoff.php"><img src="img/log-out.png" class="log-out" name="log-out" alt="log out" /></a>
     </div>
 
-    <?php
-        
+    <h2>Productos</h2>
+    <div class="container">
+        <form action="' . $_SERVER['PHP_SELF'] . '" method="post">
+            <table id="products">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>PVP</th>
+                </tr>
+                <?php echo $htmlText; ?>
+            </table>
 
-                $res = $conexion -> query("SELECT * FROM producto");
-                
-                $table = '<h2>Productos</h2>';
-                $table .= '<div class="container">';
-                $table .= '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
-                $table .= '<table id="products">';
-                $table .= '<tr>';
-                $table .= '<th>Nombre</th>';
-                $table .= '<th>Descripción</th>';
-                $table .= '<th>PVP</th>';
-                $table .= '</tr>';
-                while($p = $res->fetch_array()) {
-                    $table .= '<tr>';
-                    $table .= '<td>'.$p['nombre_corto'].'</td>';
-                    $table .= '<td>'.$p['descripcion'].'</td>';
-                    $table .= '<td>'.$p['PVP'].'€</td>';
-                    if(isset($_SESSION['cart']) && in_array($p['cod'], $_SESSION['cart'])) {
-                        $table .= '<td><input type="checkbox" name="cart['.$p['cod'].']" checked />&nbsp;</td>';
-                    } else {
-                        $table .= '<td><input type="checkbox" name="cart['.$p['cod'].']" />&nbsp;</td>';
+            <div style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+                <strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+            </div>
+            <div class="pagination">
+                <?php
+                    if($page_no > 1){
+                        echo "<li><a href='?page_no=1'>First Page</a></li>";
+                        echo "<li><a href='?page_no=$previous_page'>Previous</a></li>";
                     }
-                    $table .= '</tr>';
-                }
-                $table .= '</table>';
-                $table .= '<div class="buttonContainer">';
-                $table .= '<input type="submit" name="buy" class="buy" value="Añadir a la cesta de la compra">';
-                $table .= '</div>';
-                $table .= '</form>';
-                $table .= '</div>';
+                    if($page_no < $total_no_of_pages) {
+                        echo "<li><a href='?page_no=$next_page'>Next</a></li>";
+                        echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                    }
+                ?>
+</ul>       </div>
 
-                echo $table;
-
-            }
-
-            $conexion -> close();
-
-        }
-
-    ?>
+            <div class="buttonContainer">
+                <input type="submit" name="buy" class="buy" value="Añadir a la cesta de la compra">
+            </div>
+        </form>
+    </div>
 
     <span><?php echo $error_message; ?></span>
 </body>
