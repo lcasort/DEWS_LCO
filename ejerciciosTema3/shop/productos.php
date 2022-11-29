4593dfec9b5fe92dd0a4375626899821ed91a6f9
lcasort <laucasort.notes@gmail.com>
2022-11-29 18:15:02 +0100

    a

--- ejerciciosTema3/shop/productos.php
+++ ejerciciosTema3/shop/productos.php
@@ -2,55 +2,107 @@
     // Iniciamos la sesión o recuperamos la anterior sesión existente.
     session_start();
 
-    //
+    // Si no hemos iniciado sesión redireccionamos a 'login.php'.
     if(!isset($_SESSION['login'])) {
         header('Location: ./login.php');
         exit();
     }
+    // Si está establecida la variable de sesión 'total', la borramos.
     if(isset($_SESSION['total'])) {
         unset($_SESSION['total']);
     }
+    // Si está establecida la variable de sesión 'paid', borramos las variables
+    // de sesión 'paid' y 'cart'.
     if(isset($_SESSION['paid'])) {
         unset($_SESSION['paid']);
         unset($_SESSION['cart']);
     }
 
+    // Activamos los errores por pantalla.
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    // Variable que usaremos para guardar el código html a mostrar.
+    $table = '';
+
+    // Abrimos la conexión con la base de datos.
     $conexion = new mysqli('localhost', 'root', '', 'ejtienda');
 
+    // Guardamos en la variable $error el código de error de la última
+    // llamada a la base de datos.
     $error = $conexion->connect_errno;
+    // Guardamos en $error_message el mensaje de error que queremos mostrar
+    // por pantalla en caso de que exista.
     $error_message = ""; 
 
+    // Si hay errores, guardamos en $error_message el mensaje de error.
     if ($error != 0) {
 
         $error_message = '<p>Error de conexión a la base de datos. Texto del error:' . $conexion->connect_error . '</p>';
-        exit();
 
     } else {
 
-        if(isset($_POST['cart']) && !empty($_POST['cart'])) {
+        // Si hemos enviado el formulario de compra...
+        if(isset($_POST['cart']) && !empty($_POST['cart']) &&
+            isset($_POST['buy']) && !empty($_POST['buy'])) {
 
+            // Seleccionamos el código de los productos enviados en el post.
+            print_r(array_diff(array_values($_SESSION['cart']), array_keys($_POST['cart'])));
             $values = '(\'' . implode("','", array_keys($_POST['cart'])) . '\')';
             $res = $conexion -> query("SELECT cod FROM producto WHERE cod IN $values");
 
+            echo '$_SESSION (inicial): '; print_r($_SESSION['cart']); echo '<br><br><br>';
+            // Iteramos sobre los productos obtenidos de la consulta...
+            $arrayRes = array();
             while($cod = $res -> fetch_array()) {
-                if(!isset($_SESSION['cart'])) {
-                    $_SESSION['cart'] = array();
-                    array_push($_SESSION['cart'], $cod['cod']);
-                } else {
-                    if (!in_array($cod['cod'], $_SESSION['cart'])) {
-                        array_push($_SESSION['cart'], $cod['cod']);
-                    }
-                }
+                // Si no hemos establecido la variable de sesión 'cart', la
+                // inicializamos como un array vacío y le añadimos el código
+                // del producto.
+                array_push($arrayRes, $cod['cod']);
             }
-                
+            $_SESSION['cart'] = $arrayRes;
+            
+            // Redi
             header('Location: ./cesta.php');
             exit();
 
         } else {
+            $res = $conexion -> query("SELECT * FROM producto");
+            
+            $table .= '<h2>Productos</h2>';
+            $table .= '<div class="container">';
+            $table .= '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
+            $table .= '<table id="products">';
+            $table .= '<tr>';
+            $table .= '<th>Nombre</th>';
+            $table .= '<th>Descripción</th>';
+            $table .= '<th>PVP</th>';
+            $table .= '</tr>';
+            while($p = $res->fetch_array()) {
+                $table .= '<tr>';
+                $table .= '<td>'.$p['nombre_corto'].'</td>';
+                $table .= '<td>'.$p['descripcion'].'</td>';
+                $table .= '<td>'.$p['PVP'].'€</td>';
+                if(isset($_SESSION['cart']) && in_array($p['cod'], $_SESSION['cart'])) {
+                    $table .= '<td><input type="checkbox" name="cart['.$p['cod'].']" checked />&nbsp;</td>';
+                } else {
+                    $table .= '<td><input type="checkbox" name="cart['.$p['cod'].']" />&nbsp;</td>';
+                }
+                $table .= '</tr>';
+            }
+            $table .= '</table>';
+            $table .= '<div class="buttonContainer">';
+            $table .= '<input type="submit" name="buy" class="buy" value="Añadir a la cesta de la compra">';
+            $table .= '</div>';
+            $table .= '</form>';
+            $table .= '</div>';
+
+            }
+
+            $conexion -> close();
+
+        }
 ?>
 
 <!DOCTYPE html>
@@ -68,48 +120,7 @@
         <a href="./logoff.php"><img src="img/log-out.png" class="log-out" name="log-out" alt="log out" /></a>
     </div>
 
-    <?php
-        
-
-                $res = $conexion -> query("SELECT * FROM producto");
-                
-                $table = '<h2>Productos</h2>';
-                $table .= '<div class="container">';
-                $table .= '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
-                $table .= '<table id="products">';
-                $table .= '<tr>';
-                $table .= '<th>Nombre</th>';
-                $table .= '<th>Descripción</th>';
-                $table .= '<th>PVP</th>';
-                $table .= '</tr>';
-                while($p = $res->fetch_array()) {
-                    $table .= '<tr>';
-                    $table .= '<td>'.$p['nombre_corto'].'</td>';
-                    $table .= '<td>'.$p['descripcion'].'</td>';
-                    $table .= '<td>'.$p['PVP'].'€</td>';
-                    if(isset($_SESSION['cart']) && in_array($p['cod'], $_SESSION['cart'])) {
-                        $table .= '<td><input type="checkbox" name="cart['.$p['cod'].']" checked />&nbsp;</td>';
-                    } else {
-                        $table .= '<td><input type="checkbox" name="cart['.$p['cod'].']" />&nbsp;</td>';
-                    }
-                    $table .= '</tr>';
-                }
-                $table .= '</table>';
-                $table .= '<div class="buttonContainer">';
-                $table .= '<input type="submit" name="buy" class="buy" value="Añadir a la cesta de la compra">';
-                $table .= '</div>';
-                $table .= '</form>';
-                $table .= '</div>';
-
-                echo $table;
-
-            }
-
-            $conexion -> close();
-
-        }
-
-    ?>
+    <?php echo $table; ?>
 
     <span><?php echo $error_message; ?></span>
 </body>
