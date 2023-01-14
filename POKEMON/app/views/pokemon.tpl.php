ec566ba3b707a7137800a44b9b7b100d7a215795
laucasort <laucastillo99@gmail.com>
2023-01-14 18:42:03 +0100

    View pokemons css done.

--- POKEMON/app/views/pokemon.tpl.php
+++ POKEMON/app/views/pokemon.tpl.php
@@ -1,24 +1,61 @@
 <?php  require_once('./app/views/inc/header.php'); ?>
 
-<div>
-    <h1>Pokemons</h1>
-</div>
-<div class="pokemon_container">
-    <?php 
-    // Cogemos el id del pokemon a mostrar y lo almacenamos en $no.
-    $no = array_keys($data)[0];
-    ?>
-    <div class="pic_pokemon"><img src="<?php echo $data[$no]['pic']; ?>"></div>
-    <div class="id_pokemon"><?php echo $no; ?></div>
-    <div class="name_pokemon"><?php echo $data[$no]['name']; ?></div>
-    <div class="types_pokemon">
-    <?php foreach($data[$no]['types'] as $type): ?>
-        <a href="./?controller=PokemonType&method=list&type=<?php echo $type; ?>" class="type_link">
-            <div id="types" class="<?php echo $type; ?>"><?php echo $type; ?></div>
-        </a>
-    <?php endforeach; ?>
+<div class="main_container">
+    <div class="pokemon_grid_container">
+        <?php 
+        // Cogemos el id del pokemon a mostrar y lo almacenamos en $no.
+        $no = array_keys($data)[0];
+        ?>
+        <div class="id_pokemon">
+            <div class="item_name"><h3>No</h3></div>
+            <div class="info"><?php echo $no; ?></div>
+        </div>
+        <div class="name_pokemon">
+            <div class="item_name"><h3>Name</h3></div>
+            <div class="info"><?php echo $data[$no]['name']; ?></div>
+        </div>
+        <div class="pic_pokemon"><img src="<?php echo $data[$no]['pic']; ?>"></div>
+        <div class="types_pokemon">
+        <div class="item_name"><h3>
+        <?php if(count($data[$no]['types'])>1) { ?>
+            Types
+        <?php } else { ?>
+            Type
+        <?php } ?>
+        </h3></div>
+        <?php foreach($data[$no]['types'] as $type): ?>
+            <a href="./?controller=PokemonType&method=list&type=<?php echo $type; ?>" class="type_link">
+                <div id="type" class="<?php echo $type; ?>"><?php echo $type; ?></div>
+            </a>
+        <?php endforeach; ?>
+        </div>
+        <div class="stats_pokemon">
+            <div>
+                <div class="item_name"><h3>HP</h3></div>
+                <div class="hp"><?php echo $data[$no]['hp']; ?></div>
+            </div>
+            <div>
+                <div class="item_name"><h3>Attack</h3></div>
+                <div class="att"><?php echo $data[$no]['att']; ?></div>
+            </div>
+            <div>
+                <div class="item_name"><h3>Defence</h3></div>
+                <div class="def"><?php echo $data[$no]['def']; ?></div>
+            </div>
+            <div>
+                <div class="item_name"><h3>Special Attack</h3></div>
+                <div class="s_att"><?php echo $data[$no]['s_att']; ?></div>
+            </div>
+            <div>
+                <div class="item_name"><h3>Special Defence</h3></div>
+                <div class="s_def"><?php echo $data[$no]['s_def']; ?></div>
+            </div>
+            <div>
+                <div class="item_name"><h3>Speed</h3></div>
+                <div class="spd"><?php echo $data[$no]['spd']; ?></div>
+            </div>
+        </div>
     </div>
-    <div><?php echo $data[$no]['hp']; ?></div>
 </div>
 
 <?php require_once('./app/views/inc/footer.php'); ?>
