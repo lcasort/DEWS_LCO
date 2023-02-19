98f1f301acc02b5203f5a99fcedd493d73f83059
laucasort <laucastillo99@gmail.com>
2023-02-19 17:02:53 +0100

    Player factory and seeder done.

--- DarkSouls/database/migrations/2023_02_17_125828_create_players_table.php
+++ DarkSouls/database/migrations/2023_02_17_125828_create_players_table.php
@@ -20,9 +20,12 @@ return new class extends Migration
             $table->string('nick');
             $table->string('email')->unique();
             $table->enum('pic', [
-                'https://cdn.pixabay.com/photo/2022/12/10/01/43/woman-7646427_1280.png',
-                'https://cdn.pixabay.com/photo/2022/12/10/01/40/woman-7646421_1280.png',
-                'https://cdn.pixabay.com/photo/2022/12/10/01/47/woman-7646432_1280.png'
+                'https://cdn.pixabay.com/photo/2022/12/10/01/50/woman-7646435_960_720.png',
+                'https://cdn.pixabay.com/photo/2022/12/10/01/35/woman-7646415_960_720.png',
+                'https://cdn.pixabay.com/photo/2022/12/10/01/43/woman-7646427_960_720.png',
+                'https://cdn.pixabay.com/photo/2022/12/10/01/47/woman-7646432_960_720.png',
+                'https://cdn.pixabay.com/photo/2022/12/10/16/03/crying-7647358_960_720.png',
+                'https://cdn.pixabay.com/photo/2022/12/10/01/40/woman-7646421_960_720.png'
             ]);
             $table->timestamps();
         });
