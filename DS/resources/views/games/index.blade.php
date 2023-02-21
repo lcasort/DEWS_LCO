64f1f9a50c30dec19a2fe3f28d52b296ffccaadb
lcasort <laucasort.notes@gmail.com>
2023-02-21 18:27:19 +0100

    delete

--- DS/resources/views/games/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.layout')
-
-@section('title', 'Games')
-
-@section('content')
-    <h1>Vista principal del listado de partidas.</h1>
-@endsection
--- DS/resources/views/games/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.layout')
-
-@section('title', 'Game')
-
-@section('content')
-<h1>Vista de la partida: {{$id}}.</h1>
-@endsection
