56ddbb6de5cab95f6c43b44b01444b1eed431594
lcasort <laucasort.notes@gmail.com>
2023-02-22 19:11:44 +0100

    Carousel done.

--- DarkSouls/resources/views/index.blade.php
+++ DarkSouls/resources/views/index.blade.php
@@ -6,16 +6,70 @@
 <img src="{{asset('img/dark-souls-brand_banner.jpg')}}" alt="Dark Souls Banner"
 class="img-fluid">
 
-<div class="quote-container my-5">
+{{-- <div class="quote-container my-5">
     <figure class="text-center">
         <blockquote class="blockquote">
-          <p>"Once, the Lord of Light banished Dark, and all that stemmed from humanity. And men assumed a fleeting form. These are the roots of our world. Men are props on the stage of life, and no matter how tender, how exquisite... A lie will remain a lie!"</p>
+        <p>"Once, the Lord of Light banished Dark, and all that stemmed from humanity. And men assumed a fleeting form. These are the roots of our world. Men are props on the stage of life, and no matter how tender, how exquisite... A lie will remain a lie!"</p>
         </blockquote>
         <figcaption class="blockquote-footer">
-          <cite title="Source Title">Aldia, Scholar of the First Sin</cite>
+        <cite title="Source Title">Aldia, Scholar of the First Sin</cite>
         </figcaption>
     </figure>
-</div>
+</div> --}}
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="d-block w-100">
+                <div class="quote-container my-5 px-5">
+                    <figure class="text-center">
+                        <blockquote class="blockquote">
+                        <p>"Once, the Lord of Light banished Dark, and all that stemmed from humanity. And men assumed a fleeting form. These are the roots of our world. Men are props on the stage of life, and no matter how tender, how exquisite... A lie will remain a lie!"</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">
+                        <cite title="Source Title">Aldia, Scholar of the First Sin</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="d-block w-100">
+                <div class="quote-container my-5 px-5">
+                    <figure class="text-center">
+                        <blockquote class="blockquote">
+                        <p>"The dragons shall never be forgotten… We knights fought valiantly, but for every one of them, we lost three score of our own. Exhiliration, pride, hatred, rage… The dragons teased out our dearest emotions. …Thou will understand, one day. At thy twilight, old thoughts return, in great waves of nostalgia."</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">
+                        <cite title="Source Title">Hawkeye Gough</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="d-block w-100">
+                <div class="quote-container my-5 px-5">
+                    <figure class="text-center">
+                        <blockquote class="blockquote">
+                        <p>"Choose thy fate alone. Seize it with thine own hands. All the more, should thy fate entail such foul betrayal."</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">
+                        <cite title="Source Title">Ludleth of Courland</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
 <div class="contenerdor-index py-5 text-center">
     @if (Route::has('login'))
--- DarkSouls/resources/views/layouts/layout.blade.php
+++ DarkSouls/resources/views/layouts/layout.blade.php
@@ -7,6 +7,9 @@
     <title>@yield('title')</title>
     <!-- favicon -->
     <!-- styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
